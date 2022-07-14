e08ea7197d6d993b376e8969ef47dfa5a4b8c1af
Stefanos <stefanos@zimmerman.team>
2022-07-14 16:40:29 +0300

    feat: add rows param to data themes raw data

--- src/controllers/data-themes/raw-data.controller.ts
+++ src/controllers/data-themes/raw-data.controller.ts
@@ -1,5 +1,5 @@
 import {inject} from '@loopback/core';
-import {get, Request, response, RestBindings} from '@loopback/rest';
+import {get, param, Request, response, RestBindings} from '@loopback/rest';
 import axios, {AxiosResponse} from 'axios';
 import _ from 'lodash';
 import {mapTransform} from 'map-transform';
@@ -22,9 +22,11 @@ export class DataThemesRawDataController {
 
   @get('/data-themes/raw-data/investment-signed')
   @response(200)
-  investmentSigned(): object {
+  investmentSigned(@param.query.string('rows') rows: string): object {
     return axios
-      .get(`${urls.vgrantPeriods}/?${investmentSigned.select}&${generic.rows}`)
+      .get(
+        `${urls.vgrantPeriods}/?${investmentSigned.select}&${generic.rows}${rows}`,
+      )
       .then((res: AxiosResponse) => {
         const data = _.get(res.data, investmentSigned.dataPath, []).map(
           formatRawData,
@@ -41,10 +43,10 @@ export class DataThemesRawDataController {
 
   @get('/data-themes/raw-data/investment-committed')
   @response(200)
-  investmentCommitted(): object {
+  investmentCommitted(@param.query.string('rows') rows: string): object {
     return axios
       .get(
-        `${urls.vcommitments}/?${investmentCommitted.select}&${generic.rows}`,
+        `${urls.vcommitments}/?${investmentCommitted.select}&${generic.rows}${rows}`,
       )
       .then((res: AxiosResponse) => {
         const data = _.get(res.data, investmentCommitted.dataPath, []).map(
@@ -62,10 +64,10 @@ export class DataThemesRawDataController {
 
   @get('/data-themes/raw-data/investment-disbursed')
   @response(200)
-  investmentDisbursed(): object {
+  investmentDisbursed(@param.query.string('rows') rows: string): object {
     return axios
       .get(
-        `${urls.disbursements}/?${investmentDisbursed.select}&${generic.rows}`,
+        `${urls.disbursements}/?${investmentDisbursed.select}&${generic.rows}${rows}`,
       )
       .then((res: AxiosResponse) => {
         const data = _.get(res.data, investmentDisbursed.dataPath, []).map(
@@ -83,10 +85,10 @@ export class DataThemesRawDataController {
 
   @get('/data-themes/raw-data/budgets')
   @response(200)
-  budgets(): object {
+  budgets(@param.query.string('rows') rows: string): object {
     const mapper = mapTransform(budgets.mapping);
     return axios
-      .get(`${urls.budgets}/?${budgets.expand}&${generic.rows}`)
+      .get(`${urls.budgets}/?${budgets.expand}&${generic.rows}${rows}`)
       .then((res: AxiosResponse) => {
         const data = (mapper(res.data) as never[]).map(formatRawData);
         const filterOptionGroups = getDatasetFilterOptions(data);
@@ -101,11 +103,11 @@ export class DataThemesRawDataController {
 
   @get('/data-themes/raw-data/pledges-contributions')
   @response(200)
-  pledgesContributions(): object {
+  pledgesContributions(@param.query.string('rows') rows: string): object {
     const mapper = mapTransform(pledgesContributions.mapping);
     return axios
       .get(
-        `${urls.pledgescontributions}/?${pledgesContributions.expand}&${generic.rows}`,
+        `${urls.pledgescontributions}/?${pledgesContributions.expand}&${generic.rows}${rows}`,
       )
       .then((res: AxiosResponse) => {
         const data = (mapper(res.data) as never[]).map(formatRawData);
@@ -121,17 +123,19 @@ export class DataThemesRawDataController {
 
   @get('/data-themes/raw-data/allocations')
   @response(200)
-  allocations(): object {
+  allocations(@param.query.string('rows') rows: string): object {
     const mapper = mapTransform(allocations.mapping);
     return axios
-      .get(`${urls.allocations}/?${allocations.expand}&${generic.rows}`)
+      .get(
+        `${urls.allocations}/?${allocations.expand}&${generic.withcount}&${generic.rows}${rows}`,
+      )
       .then((res: AxiosResponse) => {
         const data = (mapper(res.data) as never[]).map(formatRawData);
         const filterOptionGroups = getDatasetFilterOptions(data);
         return {
           data,
           filterOptionGroups,
-          count: data.length,
+          count: _.get(res.data, generic.countField, data.length),
         };
       })
       .catch(handleDataApiError);
@@ -139,9 +143,9 @@ export class DataThemesRawDataController {
 
   @get('/data-themes/raw-data/grants')
   @response(200)
-  grants(): object {
+  grants(@param.query.string('rows') rows: string): object {
     return axios
-      .get(`${urls.grantsNoCount}/?${grants.select}&${generic.rows}`)
+      .get(`${urls.grantsNoCount}/?${grants.select}&${generic.rows}${rows}`)
       .then((res: AxiosResponse) => {
         const data = _.get(res.data, grants.dataPath, []).map(formatRawData);
         const filterOptionGroups = getDatasetFilterOptions(data);
@@ -156,9 +160,9 @@ export class DataThemesRawDataController {
 
   @get('/data-themes/raw-data/eligibility')
   @response(200)
-  eligibility(): object {
+  eligibility(@param.query.string('rows') rows: string): object {
     return axios
-      .get(`${urls.eligibility}/?${eligibility.select}&${generic.rows}`)
+      .get(`${urls.eligibility}/?${eligibility.select}&${generic.rows}${rows}`)
       .then((res: AxiosResponse) => {
         const data = _.get(res.data, eligibility.dataPath, []).map(
           formatRawData,
