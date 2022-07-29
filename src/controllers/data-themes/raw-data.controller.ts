b00ead52680c10a063ff027e858b7be3faa42028
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2022-07-29 16:32:35 +0300

    feat: split data calls bigger than 1000 rows

--- src/controllers/data-themes/raw-data.controller.ts
+++ src/controllers/data-themes/raw-data.controller.ts
@@ -1,21 +1,16 @@
 import {inject} from '@loopback/core';
 import {get, param, Request, response, RestBindings} from '@loopback/rest';
-import axios, {AxiosResponse} from 'axios';
-import _ from 'lodash';
-import {mapTransform} from 'map-transform';
 import allocations from '../../config/mapping/data-themes/raw-data/allocations.json';
 import budgets from '../../config/mapping/data-themes/raw-data/budgets.json';
 import eligibility from '../../config/mapping/data-themes/raw-data/eligibility.json';
-import generic from '../../config/mapping/data-themes/raw-data/generic.json';
 import grants from '../../config/mapping/data-themes/raw-data/grants.json';
 import investmentCommitted from '../../config/mapping/data-themes/raw-data/investment-committed.json';
 import investmentDisbursed from '../../config/mapping/data-themes/raw-data/investment-disbursed.json';
 import investmentSigned from '../../config/mapping/data-themes/raw-data/investment-signed.json';
 import pledgesContributions from '../../config/mapping/data-themes/raw-data/pledges-contributions.json';
 import urls from '../../config/urls/index.json';
-import {formatRawData} from '../../utils/data-themes/formatRawData';
-import {getDatasetFilterOptions} from '../../utils/data-themes/getDatasetFilterOptions';
-import {handleDataApiError} from '../../utils/dataApiError';
+import {getRawData} from '../../utils/data-themes/getRawData';
+import {getRawDataWithMapper} from '../../utils/data-themes/getRawDataWithMapper';
 
 export class DataThemesRawDataController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
@@ -23,163 +18,76 @@ export class DataThemesRawDataController {
   @get('/data-themes/raw-data/investment-signed')
   @response(200)
   investmentSigned(@param.query.string('rows') rows: string): object {
-    return axios
-      .get(
-        `${urls.vgrantPeriods}/?${investmentSigned.select}&${generic.withcount}&${generic.rows}${rows}`,
-      )
-      .then((res: AxiosResponse) => {
-        const data = _.get(res.data, investmentSigned.dataPath, []).map(
-          formatRawData,
-        );
-        const filterOptionGroups = getDatasetFilterOptions(data);
-        return {
-          data,
-          filterOptionGroups,
-          count: _.get(res.data, generic.countField, data.length),
-        };
-      })
-      .catch(handleDataApiError);
+    return getRawData(
+      `${urls.vgrantPeriods}/?${investmentSigned.select}`,
+      rows,
+      investmentSigned,
+    );
   }
 
   @get('/data-themes/raw-data/investment-committed')
   @response(200)
   investmentCommitted(@param.query.string('rows') rows: string): object {
-    return axios
-      .get(
-        `${urls.vcommitments}/?${investmentCommitted.select}&${generic.withcount}&${generic.rows}${rows}`,
-      )
-      .then((res: AxiosResponse) => {
-        const data = _.get(res.data, investmentCommitted.dataPath, []).map(
-          formatRawData,
-        );
-        const filterOptionGroups = getDatasetFilterOptions(data);
-        return {
-          data,
-          filterOptionGroups,
-          count: _.get(res.data, generic.countField, data.length),
-        };
-      })
-      .catch(handleDataApiError);
+    return getRawData(
+      `${urls.vcommitments}/?${investmentCommitted.select}`,
+      rows,
+      investmentCommitted,
+    );
   }
 
   @get('/data-themes/raw-data/investment-disbursed')
   @response(200)
   investmentDisbursed(@param.query.string('rows') rows: string): object {
-    return axios
-      .get(
-        `${urls.disbursements}/?${investmentDisbursed.select}&${generic.withcount}&${generic.rows}${rows}`,
-      )
-      .then((res: AxiosResponse) => {
-        const data = _.get(res.data, investmentDisbursed.dataPath, []).map(
-          formatRawData,
-        );
-        const filterOptionGroups = getDatasetFilterOptions(data);
-        return {
-          data,
-          filterOptionGroups,
-          count: _.get(res.data, generic.countField, data.length),
-        };
-      })
-      .catch(handleDataApiError);
+    return getRawData(
+      `${urls.disbursements}/?${investmentDisbursed.select}`,
+      rows,
+      investmentDisbursed,
+    );
   }
 
   @get('/data-themes/raw-data/budgets')
   @response(200)
   budgets(@param.query.string('rows') rows: string): object {
-    const mapper = mapTransform(budgets.mapping);
-    return axios
-      .get(
-        `${urls.budgets}/?${budgets.expand}&${generic.withcount}&${generic.rows}${rows}`,
-      )
-      .then((res: AxiosResponse) => {
-        const data = (mapper(res.data) as never[]).map(formatRawData);
-        const filterOptionGroups = getDatasetFilterOptions(data);
-        return {
-          data,
-          filterOptionGroups,
-          count: _.get(res.data, generic.countField, data.length),
-        };
-      })
-      .catch(handleDataApiError);
+    return getRawDataWithMapper(
+      `${urls.budgets}/?${budgets.expand}`,
+      rows,
+      budgets,
+    );
   }
 
   @get('/data-themes/raw-data/pledges-contributions')
   @response(200)
   pledgesContributions(@param.query.string('rows') rows: string): object {
-    const mapper = mapTransform(pledgesContributions.mapping);
-    return axios
-      .get(
-        `${urls.pledgescontributions}/?${pledgesContributions.expand}&${generic.withcount}&${generic.rows}${rows}`,
-      )
-      .then((res: AxiosResponse) => {
-        const data = (mapper(res.data) as never[]).map(formatRawData);
-        const filterOptionGroups = getDatasetFilterOptions(data);
-        return {
-          data,
-          filterOptionGroups,
-          count: _.get(res.data, generic.countField, data.length),
-        };
-      })
-      .catch(handleDataApiError);
+    return getRawDataWithMapper(
+      `${urls.pledgescontributions}/?${pledgesContributions.expand}`,
+      rows,
+      pledgesContributions,
+    );
   }
 
   @get('/data-themes/raw-data/allocations')
   @response(200)
   allocations(@param.query.string('rows') rows: string): object {
-    const mapper = mapTransform(allocations.mapping);
-    return axios
-      .get(
-        `${urls.allocations}/?${allocations.expand}&${generic.withcount}&${generic.rows}${rows}`,
-      )
-      .then((res: AxiosResponse) => {
-        const data = (mapper(res.data) as never[]).map(formatRawData);
-        const filterOptionGroups = getDatasetFilterOptions(data);
-        return {
-          data,
-          filterOptionGroups,
-          count: _.get(res.data, generic.countField, data.length),
-        };
-      })
-      .catch(handleDataApiError);
+    return getRawDataWithMapper(
+      `${urls.allocations}/?${allocations.expand}`,
+      rows,
+      allocations,
+    );
   }
 
   @get('/data-themes/raw-data/grants')
   @response(200)
   grants(@param.query.string('rows') rows: string): object {
-    return axios
-      .get(
-        `${urls.grantsNoCount}/?${grants.select}&${generic.withcount}&${generic.rows}${rows}`,
-      )
-      .then((res: AxiosResponse) => {
-        const data = _.get(res.data, grants.dataPath, []).map(formatRawData);
-        const filterOptionGroups = getDatasetFilterOptions(data);
-        return {
-          data,
-          filterOptionGroups,
-          count: _.get(res.data, generic.countField, data.length),
-        };
-      })
-      .catch(handleDataApiError);
+    return getRawData(`${urls.grantsNoCount}/?${grants.select}`, rows, grants);
   }
 
   @get('/data-themes/raw-data/eligibility')
   @response(200)
   eligibility(@param.query.string('rows') rows: string): object {
-    return axios
-      .get(
-        `${urls.eligibility}/?${eligibility.select}&${generic.withcount}&${generic.rows}${rows}`,
-      )
-      .then((res: AxiosResponse) => {
-        const data = _.get(res.data, eligibility.dataPath, []).map(
-          formatRawData,
-        );
-        const filterOptionGroups = getDatasetFilterOptions(data);
-        return {
-          data,
-          filterOptionGroups,
-          count: _.get(res.data, generic.countField, data.length),
-        };
-      })
-      .catch(handleDataApiError);
+    return getRawData(
+      `${urls.eligibility}/?${eligibility.select}`,
+      rows,
+      eligibility,
+    );
   }
 }
---
+++ src/utils/data-themes/getRawDataWithMapper.ts
@@ -0,0 +1,50 @@
+import axios, {AxiosPromise, AxiosResponse} from 'axios';
+import _ from 'lodash';
+import {mapTransform} from 'map-transform';
+import generic from '../../config/mapping/data-themes/raw-data/generic.json';
+import {formatRawData} from '../../utils/data-themes/formatRawData';
+import {getDatasetFilterOptions} from '../../utils/data-themes/getDatasetFilterOptions';
+import {handleDataApiError} from '../../utils/dataApiError';
+
+export function getRawDataWithMapper(
+  baseUrl: string,
+  rows: string,
+  mappingConfig: any,
+): object {
+  const mapper = mapTransform(mappingConfig.mapping);
+  const calls: AxiosPromise[] = [];
+  const rowsInt = parseInt(rows, 10);
+  if (rowsInt > 1000) {
+    const reps = Math.ceil(rowsInt / 1000);
+    for (let i = 0; i < reps; i++) {
+      const skip = i * 1000;
+      let localRows = 1000;
+      if (i === reps - 1) {
+        localRows = rowsInt - i * 1000;
+      }
+      calls.push(
+        axios.get(
+          `${baseUrl}&${generic.withcount}&$skip=${skip}&${generic.rows}${localRows}`,
+        ),
+      );
+    }
+  } else {
+    calls.push(
+      axios.get(`${baseUrl}&${generic.withcount}&${generic.rows}${rows}`),
+    );
+  }
+  return Promise.all(calls)
+    .then((responses: AxiosResponse[]) => {
+      let data: {[key: string]: string | number | boolean}[] = [];
+      responses.forEach((res: AxiosResponse) => {
+        data = [...data, ...(mapper(res.data) as never[]).map(formatRawData)];
+      });
+      const filterOptionGroups = getDatasetFilterOptions(data);
+      return {
+        data,
+        filterOptionGroups,
+        count: _.get(responses[0].data, generic.countField, data.length),
+      };
+    })
+    .catch(handleDataApiError);
+}
