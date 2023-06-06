d3f0d8c51366feecedebb7944ad8b67c673ed74c
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2023-06-06 13:40:09 +0300

    chore: hide board approval date

--- src/controllers/fundingrequests.controller.ts
+++ src/controllers/fundingrequests.controller.ts
@@ -65,7 +65,7 @@ export class FundingRequestsController {
                   portfolioCategory: item.portfolioCategory,
                   children: item.items.map((subitem: any) => ({
                     gac: moment(subitem.gacmeeting).format('MMM YY'),
-                    board: moment(subitem.boardApproval).format('MMM YY'),
+                    // board: moment(subitem.boardApproval).format('MMM YY'),
                     grant: subitem.IPGrantNumber,
                     start: moment(subitem.IPStartDate).format('DD-MM-YYYY'),
                     end: moment(subitem.IPEndDate).format('DD-MM-YYYY'),
