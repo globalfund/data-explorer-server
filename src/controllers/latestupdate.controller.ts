441789513b9a7f1f0491df0f0d4a1d6bc5c0ed66
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2024-10-04 12:19:55 +0300

    feat: datasets latest update date

--- src/controllers/latestupdate.controller.ts
+++ src/controllers/latestupdate.controller.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 import _ from 'lodash';
 import moment from 'moment';
 import LatestUpdateMapping from '../config/mapping/latestupdate/index.json';
+import urls from '../config/urls/index.json';
 
 export class LatestUpdateController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
@@ -11,24 +12,17 @@ export class LatestUpdateController {
   @get('/latest-update')
   @response(200)
   async latestUpdate() {
-    const responses = await Promise.all(
-      LatestUpdateMapping.datasets.map(dataset => axios.get(dataset.url)),
-    );
+    const response = await axios.get(urls.DATASET_INFORMATION);
+    const rawData = _.get(response.data, LatestUpdateMapping.dataPath, []);
 
-    const data = responses.map((response, index) => {
-      const updated = _.get(
-        response,
-        `data.${LatestUpdateMapping.dataPath}[${LatestUpdateMapping.updated}]`,
-        '',
-      );
-      const created = _.get(
-        response,
-        `data.${LatestUpdateMapping.dataPath}[${LatestUpdateMapping.created}]`,
-        '',
-      );
+    const data = LatestUpdateMapping.datasets.map(dataset => {
+      const fDataset = _.find(rawData, {
+        [LatestUpdateMapping.dataset]: dataset.datasetLabel,
+      });
+      const updated = _.get(fDataset, `[${LatestUpdateMapping.updated}]`, '');
       return {
-        name: _.get(LatestUpdateMapping.datasets, `[${index}].name`, ''),
-        date: moment(updated ?? created).format('DD MMMM YYYY'),
+        name: dataset.name,
+        date: moment(updated).format('DD MMMM YYYY'),
       };
     });
 
